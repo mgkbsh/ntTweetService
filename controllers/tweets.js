303070cce2b60e3d0a5f0466505d509ecf00dbce
mgkbsh <mkobashi@brandeis.edu>
2018-04-05 23:12:09 -0400

    Barebones service is done

---
+++ controllers/tweets.js
@@ -0,0 +1,154 @@
+var models  = require('../models');
+var sequelize = require('sequelize');
+
+// TODO: Parse tweet content in background and insert into Hashtag and Mention.
+module.exports.tweet =  async (req, res) => {
+  res.status(200).send('success');
+
+  try {
+    await models.Tweet.create({
+        content: req.query.content,
+        userId: req.query.id,
+        parentId: req.query.parentId
+    });
+
+    await models.User.update(
+        { numTweets: sequelize.literal(`"Users"."numTweets" + 1`) },
+        { where: { id: req.query.id }
+    });
+
+  } catch (err) {
+
+  }
+};
+
+// Example return JSON:
+// {
+//     "content": "hey",
+//     "createdAt": "2018-04-04T22:38:40.178Z",
+//     "user": {
+//         "fullName": "yoyo yoyo",
+//         "username": "yoyo"
+//     }
+// }
+module.exports.getTweet =  async (req, res) => {
+  var id = req.query.id
+
+  try {
+    var tweet = await models.Tweet.findOne({
+      where: { id: id },
+      include: [{
+        model: models.User,
+        as: 'user',
+        attributes: ['username', 'fname', 'lname']
+      }],
+      attributes: ['content', 'createdAt']
+    });
+
+    res.json(tweet);
+
+  } catch (err) {
+    res.status(404).send(err);
+  }
+};
+
+module.exports.like = async (req, res) => {
+  res.status(200).send('success');
+
+  try {
+    await models.Like.create({
+      userId: req.query.userId,
+      tweetId: req.query.tweetId
+    })
+
+  } catch (err) {
+
+  }
+};
+
+// Example return JSON:
+// [{
+//  "createdAt": "2018-03-11T07:56:36.176Z",
+//  "user": {
+//    "fullName" : "Bob Builder"
+//    "username": "bob_builder"
+//  }
+// },
+// {
+//  "createdAt": "2018-03-11T07:56:36.176Z",
+//  "user": {
+//    "fullName" : "Dona Explorer"
+//    "username": "dora_explorer"
+//  }
+// }]
+module.exports.getLikes = async (req, res) => {
+  var id = req.query.id
+
+  try {
+    var users = await models.Like.findAll({
+      where: { tweetId: id },
+      include: [{
+        model: models.User,
+        as: 'user',
+        attributes: ['username']
+      }],
+      attributes: ['createdAt']
+    });
+
+    res.json(JSON.parse(JSON.stringify(users)));
+
+  } catch (err) {
+    res.status(404).send(err);
+  }
+};
+
+module.exports.retweet = async (req, res) => {
+  res.status(200).send('success');
+
+  try {
+    await models.Tweet.create({
+        content: "",
+        userId: req.query.userId,
+        originalId: req.query.tweetId
+    })
+
+  } catch (err) {
+
+  }
+};
+
+// Example return JSON:
+// [{
+//  "createdAt": "2018-03-11T07:56:36.176Z",
+//  "user": {
+//    "fullName" : "Bob Builder"
+//    "username": "bob_builder"
+//  }
+// },
+// {
+//  "createdAt": "2018-03-11T07:56:36.176Z",
+//  "user": {
+//    "fullName" : "Dora explorer"
+//    "username": "dora_explorer"
+//  }
+// }]
+module.exports.getRetweets = async (req, res) => {
+  var id = req.query.id
+
+  try {
+    var users = await models.Tweet.findAll({
+      where: { originalId: req.query.id },
+      include: [{
+        model: models.User,
+        as: 'user',
+        attributes: ['username']
+      }],
+      attributes: ['createdAt']
+    });
+
+    res.json(JSON.parse(JSON.stringify(users)));
+
+  } catch (err) {
+    res.status(404).send(err);
+  }
+};
--- index.js
+++ index.js
@@ -1,36 +1,27 @@
-// server.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4567;
 
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+/* ===========BODY_PARSER=========== */
+const bodyParser = require('body-parser');
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application/json
 app.use(bodyParser.json());
+// Parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-var port = process.env.PORT || 3000;        // set our port
+/* =============ROUTES============= */
+const tweets = require('./controllers/tweets')
+const router = express.Router();
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
+router.get('/tweet', tweets.getTweet);
+router.post('/tweet', tweets.tweet);
+router.get('/likes', tweets.getLikes);
+router.post('/like', tweets.like);
+router.get('/retweets', tweets.getRetweets);
+router.post('/retweet', tweets.retweet);
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', (req, res) => {
-    res.json({ message: 'hooray! welcome to the tweet service!' });
-});
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
 app.use('/', router);
 
-// START THE SERVER
-// =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
