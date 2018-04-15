20e87714715a95b48a03ffed0d7d59342982edd9
Kevin <kwang30@gmail.com>
2018-04-15 00:17:59 -0400

    added printing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4567;
+console.log(port)
 
 /* ===========BODY_PARSER=========== */
 const bodyParser = require('body-parser');
@@ -15,19 +16,19 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 const tweets = require('./controllers/tweets')
 const timeline = require('./controllers/timeline')
 const router = express.Router();
-
-router.get('/tweet', tweets.getTweet);
-router.post('/tweet', tweets.tweet);
-router.get('/likes', tweets.getLikes);
-router.post('/like', tweets.like);
-router.post('/unlike', tweets.unlike);
-router.get('/retweets', tweets.getRetweets);
-router.post('/retweet', tweets.retweet);
-router.get('/timeline/followees', timeline.getFolloweeTimeline);
-router.get('/timeline/global', timeline.getGlobalTimeline);
-router.get('/timeline/user', timeline.getUserTimeline);
-
-app.use('/', router);
+//
+// router.get('/tweet', tweets.getTweet);
+// router.post('/tweet', tweets.tweet);
+// router.get('/likes', tweets.getLikes);
+// router.post('/like', tweets.like);
+// router.post('/unlike', tweets.unlike);
+// router.get('/retweets', tweets.getRetweets);
+// router.post('/retweet', tweets.retweet);
+// router.get('/timeline/followees', timeline.getFolloweeTimeline);
+// router.get('/timeline/global', timeline.getGlobalTimeline);
+// router.get('/timeline/user', timeline.getUserTimeline);
+//
+// app.use('/', router);
 app.get('/',function(req, res, next){
   res.json({"test":"test"})
 })
