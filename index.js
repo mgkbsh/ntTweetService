8a4e9f4ca2e86a2500b49fb1bcb3767f29471bac
Kevin <kwang30@gmail.com>
2018-04-15 18:01:50 -0400

    fix error overriding config var....

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 /* =============ROUTES============= */
-// const tweets = require('./controllers/tweets')
+const tweets = require('./controllers/tweets')
 const timeline = require('./controllers/timeline')
 const router = express.Router();
-//
-// router.get('/tweet', tweets.getTweet);
-// router.post('/tweet', tweets.tweet);
-// router.get('/likes', tweets.getLikes);
-// router.post('/like', tweets.like);
-// router.post('/unlike', tweets.unlike);
-// router.get('/retweets', tweets.getRetweets);
-// router.post('/retweet', tweets.retweet);
+
+router.get('/tweet', tweets.getTweet);
+router.post('/tweet', tweets.tweet);
+router.get('/likes', tweets.getLikes);
+router.post('/like', tweets.like);
+router.post('/unlike', tweets.unlike);
+router.get('/retweets', tweets.getRetweets);
+router.post('/retweet', tweets.retweet);
 router.get('/timeline/followees', timeline.getFolloweeTimeline);
 router.get('/timeline/global', timeline.getGlobalTimeline);
 router.get('/timeline/user', timeline.getUserTimeline);
