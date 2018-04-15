a3f886adfa24666581aa81791b2bebf37ae8e358
Kevin <kwang30@gmail.com>
2018-04-15 17:47:07 -0400

    trying again

--- models/index.js
+++ models/index.js
@@ -4,13 +4,10 @@ const Sequelize = require('sequelize');
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(`${__dirname}/../config/config.json`)[env];
-console.log(config)
 const db = {};
 
 var sequelize;
 if (config.use_env_variable) {
-  console.log(config.use_env_variable)
-  console.log(process.env[config.use_env_variable])
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
